454104b781c04e2615fbff236d4bc8181f6e0e14
Michael Ali <michael.ali1995@gmail.com>
2018-10-23 03:29:06 -0400

    updated firebase push to new structure, included restart command for rpi

---
+++ rpi_command.py
@@ -0,0 +1,6 @@
+def restart():
+    command = "/usr/bin/sudo /sbin/shutdown -r now"
+    import subprocess
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    print output
