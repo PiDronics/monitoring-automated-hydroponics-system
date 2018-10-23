3ebb596f38f9a217e56f70c6edf4753a1f2ca972
Michael Ali <michael.ali1995@gmail.com>
2018-10-23 03:36:41 -0400

    PEP8 linting

--- rpi_command.py
+++ rpi_command.py
@@ -3,4 +3,4 @@ def restart():
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    print output
+    print(output)
