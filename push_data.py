4bc6f83b39e5ee25c5cbc5f411e5e5a7e3dc7dea
Michael Ali <michael.ali1995@gmail.com>
2018-10-23 01:28:08 -0400

    listening to sensors

---
+++ push_data.py
@@ -0,0 +1,22 @@
+import pyrebase
+from time import sleep
+import env
+from poll_sensors import poll
+
+firebase = pyrebase.initialize_app(
+    {
+        "apiKey": env.API_KEY,
+        "authDomain": env.AUTH_DOMAIN,
+        "databaseURL": env.DATABASE_URL,
+        "storageBucket": env.STORAGE_BUCKET
+    }
+)
+db = firebase.database()
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password(env.EMAIL, env.PASS)
+
+while True:
+    result = poll()
+    db.child("systems").child("pi-1").child("data-dev").push(result, user["idToken"])
+    sleep(120) # Polls and pushes every 2m to reduce overhead
+    
