e6f07a2aa2b417d7b2c4e3eee1b06a6c3bace30d
Michael Ali <michael.ali1995@gmail.com>
2018-11-29 02:12:55 -0400

    abstracting sensor code, improved modularity

--- sensor_unittests.py
+++ sensor_unittests.py
@@ -1,13 +1,13 @@
-from unittest import TestCase
-import poll_sensors
+import unittest 
+import poll_sensors, temp, ph
 
-class TestUnitTests(TestCase):
+class TestUnitTests(unittest.TestCase):
     def setUp(self):
         print("Running the setup before the test")
     
 
     def test_dataTypes(self):
-        self.assertIsInstance(poll_sensors.instance, DHT11)
+        self.assertIsInstance(poll_sensors.instance, temp.TemperatureSensor)
         # test if result is an object
         # test if result.temperature and result.humidity are floats
         
@@ -18,8 +18,8 @@ class TestUnitTests(TestCase):
     # def test_libraries(self):
 
     def test_equal(self):
-        param1 = 'wee'
-        param2 = 'wee'
+        param1 = ' '
+        param2 = ' '
         self.assertEqual(param1, param2)
 
     def tearDown(self):
